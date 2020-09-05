069fcdd7a58c515f8a2ccfdb54d45c95de55623b
Jeff_Home_PC <fantasist.s@gmail.com>
2020-09-05 10:50:00 +0900

    2048

--- 2048.c
+++ 2048.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int Array[4][4];
 
+void print_array()
+{
+    char i, j;
+
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            printf("%d", Array[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 void main()
 {
-    char a;
-    a= rand();
+    char scan_dir;
+    //char new_num;
 
-    printf("%d", a);
+    while (1)
+    {
+        Array[0][0] = rand() % 3;
+        scanf("%c", &scan_dir);
 
+        if (scan_dir == 'u')
+        {
+            print_array();
+        }
+    }
 }
