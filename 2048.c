1295d6fb8b19d3f4c71a7f0e4f7765ee9e3dd835
Jeff_X44 <fantasist.s@gmail.com>
2020-09-06 14:17:00 +0900

    update

--- 2048.c
+++ 2048.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_ROW 4
-#define MAX_COL 4
+#define MAX_ROW 4 // test
+#define MAX_COL 4 // test
 
 int Array[MAX_ROW][MAX_COL];
 
@@ -10,13 +10,19 @@ void array_move(char dir)
 {
     char row, col;
     char row_buffer, col_buffer;
-    if (dir == 'd')
-    {
-        for (col = 0; col < MAX_COL; col++)
-        {
-            for (row = MAX_ROW - 1; row < 0; row--)
-            {
-                if ()
+    if (dir == 'd') {
+        for (col = 0; col < MAX_COL; col++) {
+            for (row = MAX_ROW - 1; row < 0; row--) {
+                // Remove zero
+                if (Array[row][col] == 0) {
+                    if (row == 0) {
+                    }
+                    else if (Array[row - 1][col] != 0) {
+                        Array[row][col] = Array[row - 1][col];
+                        Array[row - 1][col] = 0;
+                    }
+                }
+                // Sum
             }
         }
     }
@@ -26,10 +32,8 @@ void print_array()
 {
     char row, col;
 
-    for (row = 0; row < MAX_ROW; row++)
-    {
-        for (col = 0; col < MAX_COL; col++)
-        {
+    for (row = 0; row < MAX_ROW; row++) {
+        for (col = 0; col < MAX_COL; col++) {
             printf("%d", Array[row][col]);
         }
         printf("\n");
@@ -39,17 +43,15 @@ void print_array()
 void main()
 {
     char scan_dir;
-    //char new_num;
+    // char new_num;
 
-    while (1)
-    {
+    while (1) {
         Array[0][0] = rand() % 3;
         scanf("%c", &scan_dir);
 
         array_move(scan_dir);
 
-        if (scan_dir == 'u')
-        {
+        if (scan_dir == 'u') {
             print_array();
         }
     }
